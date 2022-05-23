ce91081f7e75c8f36f8009876441c4944af6b323
Nathan Quadros da Silva <contato@nathanqsilva.com.br>
2022-05-23 11:39:52 -0300

    Criando sistema de login

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,9 +1,59 @@
+import { useState } from "react"
+
 export default function Contato() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        assunto: "",
+        texto: ""
+    })
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        console.log(formData)
+        sessionStorage.setItem("name", formData.name)
+        sessionStorage.setItem("email", formData.email)
+        sessionStorage.setItem("assunto", formData.assunto)
+        sessionStorage.setItem("texto", formData.texto)
+    }
+
     return (
-        <body>
-            <div>
-                teste Contato
-            </div>
+        <body >
+            <form className="bg-slate-600 h-full" onSubmit={handleSubmit}>
+                <h1>Form</h1>
+                <input 
+                    type="text" 
+                    name="name"  
+                    id="name" 
+                    placeholder="name" 
+                    value={formData.name} 
+                    onChange={(value) => setFormData({...formData, name: value.target.value})} />
+                <input 
+                    type="text" 
+                    name="email" 
+                    id="email" 
+                    placeholder="email" 
+                    value={formData.email} 
+                    onChange={(value) => setFormData({...formData, email: value.target.value})}/>
+                <input 
+                    type="text" 
+                    name="assunto" 
+                    id="assunto" 
+                    placeholder="assunto" 
+                    value={formData.assunto}
+                    onChange={(value) => setFormData({...formData, assunto: value.target.value})} />
+                <textarea 
+                    name="texto" 
+                    id="texto" 
+                    value={formData.texto}
+                    onChange={(value) => setFormData({...formData, texto: value.target.value})}
+                    ></textarea>
+
+                <button type="submit">
+                    onSubmit
+                </button>
+            </form>
         </body>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react"
+
 export default function Login() {
+    
+    const [loginData, setLoginData] = useState({
+        user: "",
+        pass: "",
+        passConfirm: ""        
+    })
+
+    const handleSubmit = (event:any) => {
+        event.preventDefault()
+        sessionStorage.setItem("user", loginData.user)
+        if (loginData.pass === loginData.passConfirm) {
+            sessionStorage.setItem("pass", loginData.pass)
+        } else {
+            console.log("erro")
+        }
+    }
+
+    const handleChange = (event: any) => {
+        if (event.target.value === loginData.pass ) {
+            setLoginData({...loginData, passConfirm: event.target.value})
+        }
+    }
+
     return (
         <body>
-            texto aleatorio
+            <form className="bg-slate-500 p-11 flex flex-col items-center justify-center" onSubmit={handleSubmit}>
+                <input type="text" placeholder="User" value={loginData.user} onChange={(event) => setLoginData({...loginData, user: event.target.value})} />
+                <input type="password" placeholder="password" value={loginData.pass} onChange={(event) => setLoginData({...loginData, pass: event.target.value})} />
+                <input type="password" placeholder="confirm password" value={loginData.passConfirm} onChange={(event) => {handleChange(event)}} />
+                <button type="submit">
+                    Entrar
+                </button>
+            </form>
         </body>
     )
 }
